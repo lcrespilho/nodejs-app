c0f8aa6eb792a68282a57c5e63234a7279da1a37
Leonardo Lourenço Crespilho <lcrespilho@gmail.com>
2016-09-19 23:15:09 -0300

    coding...

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,13 @@
 module.exports = function(app) {
+  app.get('/todo', (req, res) => {
+    res.render('todo');
+  });
 
+  app.post('/todo', (req, res) => {
+
+  });
+
+  app.delete('/todo', (req, res) => {
+
+  });
 };
