edbaeeacd4e763caa6bad9a7f87a885185c3e55e
Leonardo Lourenço Crespilho <lcrespilho@gmail.com>
2016-09-20 00:59:17 -0300

    programando...

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,8 @@
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+
 module.exports = function(app) {
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
   app.post('/todo', (req, res) => {
